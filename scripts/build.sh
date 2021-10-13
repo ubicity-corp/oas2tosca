c4e08f399b3c7a1bcae7bf544fe94d3571d316b1
Chris Lauwers <lauwers@ubicity.com>
2021-10-13 16:07:56 -0700

    Add build script

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit on first failure
+set -e
+
+# Find relevant directories
+
+HERE=$(dirname "$(readlink --canonicalize "$BASH_SOURCE")")
+TOP_DIR=$(readlink --canonicalize "$HERE/..")
+WHEELS_DIR="${TOP_DIR}"/wheels
+
+# Build
+pip wheel ${TOP_DIR} --wheel-dir ${WHEELS_DIR} --only-binary :all:
+
+
+
+						      
